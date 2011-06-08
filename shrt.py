9378ff22f615c55c545a34c15fa55bdb4e85c473
Nikita Zubkov <zubchick@yandex-team.ru>
2011-06-08 23:30:19 +0400

    refactoring

--- shrt.py
+++ shrt.py
@@ -6,21 +6,24 @@ from models import Link
 app = Flask(__name__)
 app.config.from_object('config')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def main_page():
+    if request.method == 'POST':
+        return show_short_url()
+
     url =  request.args.get('url', None)
-    if url:
-        if url.find(request.host) >= 0:
-            # redirect loop protection
-            return url
-        elif len(url) < len(request.host) + 6:
-            # url already short
-            return url
-        else:
-            l = Link(url=url)
-        return "http://%s/%s" % (request.host, l.key)
-    else:
+    if not url:
         return render_template('mainpage.html')
+
+    if url.find(request.host) >= 0:
+        # redirect loop protection
+        return url
+    elif len(url) < len(request.host) + 6:
+        # url already short
+        return url
+    else:
+        l = Link(url=url)
+    return "http://%s/%s" % (request.host, l.key)
 
 @app.route('/<key>')
 def key_url(key):
@@ -33,22 +36,22 @@ def key_url(key):
     else:
         abort(404)
 
-@app.route('/short', methods=['POST'])
 def show_short_url():
     url = request.form.get('url', None)
-    if url:
-        if url.find(request.host) >= 0:
-            # redirect loop protection
-            return render_template('shorturl.html', short_url=url)
-        elif len(url) < len(request.host) + 6:
-            # url already short
-            return render_template('shorturl.html', short_url=url)
-        else:
-            l = Link(url=url)
-            return render_template('shorturl.html', link=l,
-                                   short_url="http://%s/%s" % (request.host, l.key))
-    else:
+    if not url:
         return render_template('mainpage.html', error='Input url at first')
+
+    if url.find(request.host) >= 0:
+        # redirect loop protection
+        return render_template('shorturl.html', short_url=url)
+    elif len(url) < len(request.host) + 6:
+        # url already short
+        return render_template('shorturl.html', short_url=url)
+    else:
+        l = Link(url=url)
+        return render_template('shorturl.html', link=l,
+                               short_url="http://%s/%s" % (request.host, l.key))
+
 
 if __name__ == '__main__':
     app.run(debug=app.config['DEBUG'])
